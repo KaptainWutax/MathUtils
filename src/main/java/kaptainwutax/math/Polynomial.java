d494918eb4a8c562cbf9da3f78cb4d9267002cd5
KaptainWutax <kaptainwutax@gmail.com>
2020-08-11 15:40:22 -0400

    added polynomial

---
+++ src/main/java/kaptainwutax/math/Polynomial.java
@@ -0,0 +1,136 @@
+package kaptainwutax.math;
+
+import java.util.Arrays;
+
+public final class Polynomial {
+
+	private final Rational[] coefficients;
+	private int degree = -1;
+
+	private Polynomial derivative;
+
+	public Polynomial(Rational... coefficients) {
+		this.coefficients = coefficients;
+		this.computeDegree();
+	}
+
+	public Polynomial(Rational coefficient, int exponent) {
+		this.coefficients = new Rational[exponent + 1];
+		this.coefficients[exponent] = coefficient;
+		this.degree = exponent;
+	}
+
+	private void computeDegree() {
+		for(int i = this.coefficients.length - 1; i >= 0; i--) {
+			if(this.coefficients[i].signum() != 0) {
+				this.degree = i;
+				break;
+			}
+		}
+	}
+
+	public int degree() {
+		return this.degree;
+	}
+
+	public Rational coefficient(int exponent) {
+		return this.coefficients[exponent];
+	}
+
+	public Rational[] coefficients() {
+		return Arrays.copyOf(this.coefficients, this.coefficients.length);
+	}
+
+	public Rational evaluate(Rational point) {
+		Rational result = Rational.ZERO;
+
+		for(int i = this.degree; i >= 0; i--) {
+			result = result.multiply(point).add(this.coefficients[i]);
+		}
+
+		return result;
+	}
+
+	public Polynomial differentiate() {
+		if(this.derivative != null)return this.derivative;
+		if(this.degree <= 0)return new Polynomial(Rational.ZERO, 0);
+
+		Polynomial r = new Polynomial(Rational.ZERO, this.degree - 1);
+
+		for(int e = 1; e < this.degree; e++) {
+			r.coefficients[e - 1] =  this.coefficients[e].multiply(e);
+		}
+
+		return this.derivative = r;
+	}
+
+	public Polynomial add(Polynomial other) {
+		Polynomial r = new Polynomial(Rational.ZERO, Math.max(this.degree, other.degree));
+
+		for (int i = 0; i <= r.degree; i++) {
+			if(i <= this.degree) {
+				r.coefficients[i] = this.coefficients[i];
+			}
+
+			if(i <= other.degree) {
+				r.coefficients[i] = r.coefficients[i].add(other.coefficients[i]);
+			}
+		}
+
+		r.computeDegree();
+		return r;
+	}
+
+	public Polynomial multiply(Polynomial other) {
+		Polynomial r = new Polynomial(Rational.ZERO, this.degree + other.degree);
+
+		for(int i = 0; i <= this.degree; i++) {
+			for(int j = 0; j <= other.degree; j++) {
+				r.coefficients[i + j] = r.coefficients[i + j].add(this.coefficients[i].multiply(other.coefficients[j]));
+			}
+		}
+
+		r.computeDegree();
+		return r;
+	}
+
+	public Polynomial compose(Polynomial other) {
+		Polynomial r = new Polynomial(Rational.ZERO, 0);
+
+		for(int i = this.degree; i >= 0; i--) {
+			Polynomial t = new Polynomial(this.coefficients[i], 0);
+			r = t.add(other.multiply(r));
+		}
+
+		return r;
+	}
+
+	@Override
+	public String toString() {
+		if(this.degree < 0) {
+			return "0";
+		}
+
+		StringBuilder sb = new StringBuilder();
+
+		for(int i = this.degree; i >= 0; i--) {
+			Rational c = this.coefficients[i];
+			int sign = c.signum();
+			c = c.multiply(sign);
+
+			if(i != this.degree) {
+				sb.append(sign == 1 ? " + " : " - ");
+			}
+
+			sb.append(c);
+
+			if(i != 0) {
+				sb.append("x");
+				if(i != 1)sb.append("^").append(i);
+			}
+		}
+
+		return sb.toString();
+	}
+
+}
